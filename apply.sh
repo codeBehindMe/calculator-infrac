d4031718b74d218a4de9a340f4f049fe43728c07
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2020-10-27 20:41:23 +1100

    CAL-48: Added master auth ip cidr

--- apply.sh
+++ apply.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 echo "Applying terraform"
 echo "Using project $1"
 echo "Using zone $2"
+echo "Using master auth ip cidr as $3"
 terraform validate
-terraform plan -var="project_name=$1" -var="geo_zone=$2"
-terraform apply -var="project_name=$1" -var="geo_zone=$2"
+terraform plan -var="project_name=$1" -var="geo_zone=$2" -var="master_auth_ip_cidr=$3"
+terraform apply -var="project_name=$1" -var="geo_zone=$2" -var="master_auth_ip_cidr=$3"
--- cluster.tf
+++ cluster.tf
@@ -5,6 +5,10 @@ variable "geo_zone" {
   type = string
 }
 
+variable "master_auth_ip_cidr" {
+  type = string
+}
+
 provider "google" {
   credentials = file("maker.json")
   project = var.project_name
@@ -28,6 +32,12 @@ resource "google_container_cluster" "calc_cluster" {
     cluster_ipv4_cidr_block = "192.168.0.0/16"
   }
 
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block = var.master_auth_ip_cidr
+    }
+  }
+
   addons_config {
     istio_config {
       disabled = false
@@ -40,7 +50,9 @@ resource "google_container_node_pool" "preemptible_nodes_alpha" {
   name = "pe-alpha-pool"
   location = var.geo_zone
   cluster = google_container_cluster.calc_cluster.name
+  initial_node_count = 1
 
+  node_locations = [var.geo_zone]
   autoscaling {
     max_node_count = 5
     min_node_count = 1
