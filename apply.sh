aa0b754e2fa8aacea5ea60073e45b92bb3f6b7b5
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2020-10-27 20:55:17 +1100

    CAL-35: Added auto approve signal to terraform

--- apply.sh
+++ apply.sh
@@ -6,4 +6,4 @@ echo "Using zone $2"
 echo "Using master auth ip cidr as $3"
 terraform validate
 terraform plan -var="project_name=$1" -var="geo_zone=$2" -var="master_auth_ip_cidr=$3"
-terraform apply -var="project_name=$1" -var="geo_zone=$2" -var="master_auth_ip_cidr=$3"
+terraform apply -var="project_name=$1" -var="geo_zone=$2" -var="master_auth_ip_cidr=$3" -auto-approve
